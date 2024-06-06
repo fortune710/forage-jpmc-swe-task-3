fc92b8e0c828f5cbf38ad303441c3a0ac458a953
fortune710 <fortune0208@yahoo.com>
2024-06-06 12:31:48 -0400

    completed task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FC, useState } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
@@ -8,29 +8,22 @@ interface IState {
   showGraph: boolean,
 }
 
-class App extends Component<{}, IState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      data: [],
-      showGraph: false,
-    };
+//Converted to a function component
+const App: FC = () => {
+  const [data, setData] = useState<IState["data"]>([]);
+  const [showGraph, setShowGraph] = useState<IState["showGraph"]>(false);
+
+  function renderGraph() {
+    if (!showGraph) return null
+    return (<Graph data={data}/>)
   }
 
-  renderGraph() {
-    if (this.state.showGraph) {
-      return (<Graph data={this.state.data}/>)
-    }
-  }
-
-  getDataFromServer() {
+  function getDataFromServer() {
     let x = 0;
     const interval = setInterval(() => {
       DataStreamer.getData((serverResponds: ServerRespond[]) => {
-        this.setState({
-          data: serverResponds,
-          showGraph: true,
-        });
+        setData(serverResponds)
+        setShowGraph(true)
       });
       x++;
       if (x > 1000) {
@@ -39,21 +32,20 @@ class App extends Component<{}, IState> {
     }, 100);
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          Bank Merge & Co Task 3
-        </header>
-        <div className="App-content">
-          <button className="btn btn-primary Stream-button" onClick={() => {this.getDataFromServer()}}>Start Streaming Data</button>
-          <div className="Graph">
-            {this.renderGraph()}
-          </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        Bank Merge & Co Task 3
+      </header>
+      <div className="App-content">
+        <button className="btn btn-primary Stream-button" onClick={getDataFromServer}>Start Streaming Data</button>
+        <div className="Graph">
+          {renderGraph()}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
 
 export default App;
--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,41 @@
 import { ServerRespond } from './DataStreamer';
 
+//Properties to be captured by the Grapgh
 export interface Row {
-  stock: string,
-  top_ask_price: number,
   timestamp: Date,
+  ratio: number,
+  price_abc: number,
+  price_def: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined
+}
+
+//Get price of stock by computing average of top ask and top bid
+function computeAverage(datapoint: ServerRespond) {
+  const average = (datapoint.top_ask.price + datapoint.top_bid.price) / 2
+  return average;
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverResponds: ServerRespond[]): Row {
+    const price_abc = computeAverage(serverResponds[0]);
+    const price_def = computeAverage(serverResponds[1]);
+    const ratio = price_abc / price_def
+    const upper_bound = 1 + 0.05
+    const lower_bound = 1 - 0.05
+
+    return {
+      timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp, 
+      //set timestamp if timestamp of stock abc is greater than def or vice versa
+      trigger_alert: (ratio < lower_bound || ratio > upper_bound) ?  ratio : undefined, 
+      //Trigger alert is ratio is greater than upper bound or less than lower bound
+      upper_bound, 
+      lower_bound,
+      price_abc,
+      price_def,
+      ratio
+    };
   }
 }
